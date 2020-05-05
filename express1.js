b2e326fb2d0ef048fee90579a57c94a47073230c
Shubham567 <shubhamshekhar567@gmail.com>
2020-05-05 07:33:29 +0530

    some test

--- express1.js
+++ express1.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+app.set("port", process.env.PORT || 3000);
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,4 +15,6 @@ app.get("/", function(request, response) {
 
 console.log("Reached Here");
 
-app.listen(8080);
+app.listen(app.get("port"), function() {
+    console.log("App started on port " + app.get("port"));
+});
