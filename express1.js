9d281e3b8f46dfc52644bd614e140931da37b48c
Shubham567 <shubhamshekhar567@gmail.com>
2020-05-05 12:22:17 +0530

    something working atleast

--- express1.js
+++ express1.js
@@ -1,19 +1,47 @@
 var express = require("express");
 var path = require("path");
-var app = express();
+var bodyParser  = require("body-parser");
+var logger = require("morgan");
 
+var app = express();
 app.set("port", process.env.PORT || 3000);
+
+var entries = [];
+app.locals.entries = entries;
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", function(request, response) {
-    response.render("index", {
-    message: "Hey everyone! This is my webpage. Lets see how it changed"
-
-    });
+    response.render("index");
 });
 
+app.get("/new-entry", function(request, response) {
+    response.render("new-entry");
+});
+
+app.post("/new-entry", function(request, response) {
+    if (!request.body.title || !request.body.body) {
+        response.status(400).send("Entries must have a title and a body.");
+        return;
+    }
+    entries.push({
+        title: request.body.title,
+        content: request.body.body,
+        published: new Date()
+    });
+    response.redirect("/");
+});
+
+app.use(function(request, response) {
+    response.status(404).render("404");
+});
 console.log("Reached Here");
 
 app.listen(app.get("port"), function() {
